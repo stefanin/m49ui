a15ae62b61f6f9153eba3b06e6a095a37e7922b9
stefanin <stefanin@MacBook-Pro-di-stefanin.local>
2020-12-29 18:51:35 +0100

    primo commot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { M49sSQLserviceService } from "./m49s-sqlservice.service";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { TabellaComponent } from './tabella/tabella.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabellaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    Ng2SearchPipeModule,
   ],
-  providers: [],
+  providers: [M49sSQLserviceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/tabella/tabella.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { M49sSQLserviceService } from "../m49s-sqlservice.service";
+
+@Component({
+  selector: 'tabella',
+  templateUrl: './tabella.component.html',
+  styleUrls: ['./tabella.component.css']
+})
+export class TabellaComponent implements OnInit {
+  vidSQL='0';
+  @Input() vSQL: string ;
+  @Input() colonne: string[];
+  dati: any=[];
+  filtro: string;
+
+  constructor(private qs:M49sSQLserviceService) { }
+
+  ngOnInit(): void {
+    this.qs.vidSQL=this.vidSQL;
+    this.qs.vSQL=this.vSQL;
+
+    this.qs.inviaPost().subscribe((datijson)=>{
+      this.dati=datijson;
+      console.log(datijson);
+    });
+  }
+
+}
